3b3e306837fdd3670f13eb33ee59a3406e0cfb07
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-12-12 14:59:11 +0530

    updated

--- src/pages/location/DarkCatchmentExansion.jsx
+++ src/pages/location/DarkCatchmentExansion.jsx
@@ -1,15 +1,20 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { tablueLink } from "../../tabluea/TablueLink";
+import Loader from "../../components/common/Loder";
 
 
 const DarkCatchmentExansion = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(false)
+  }, [loading])
 
   return (
     <div className="HomePageTablue">
-      <tableau-viz id="tableauViz"
+      {loading ? <Loader /> : <tableau-viz id="tableauViz"
         src={tablueLink}
-      />
+      />}
     </div>
   );
 };
--- src/pages/location/NewCityExpansion.jsx
+++ src/pages/location/NewCityExpansion.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { tablueLink } from "../../tabluea/TablueLink";
+import Loader from "../../components/common/Loder";
 
 
 const NewCityExpansion = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(false)
+  }, [loading])
   return (
     <div className="HomePageTablue">
-      <tableau-viz id="tableauViz"
+      {loading ? <Loader /> : <tableau-viz id="tableauViz"
         src={tablueLink}
-      />
+      />}
     </div>
   )
 }
